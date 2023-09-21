3a25bef66f38fad7aabc0e35699c03cfe80e1633
Sharawmnyambuu <sharawnyambuuurtnasan@gmail.com>
2023-09-21 15:21:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# newsSolution
+# spanlink
---
+++ assets/js/init.js
@@ -0,0 +1,11 @@
+/* Setting */
+function set_init(){}
+ 
+/* UI */
+function ui_init(){}
+ 
+/* Ready */
+$(function(){
+    set_init();
+    ui_init();
+});
